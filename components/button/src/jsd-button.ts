cc2310eaed367a1c158d4d9f87b982ad3d3feea7
Jayabratha Saha <js.design@outlook.com>
2019-11-18 18:51:09 +0530

    Adding full width support

--- components/button/src/jsd-button.ts
+++ components/button/src/jsd-button.ts
@@ -10,6 +10,7 @@ export class Button extends LitElement {
     @property({ type: Boolean }) tertiary = false;
     @property({ type: Boolean }) iconPrefix = false;
     @property({ type: Boolean }) iconSuffix = false;
+    @property({ type: Boolean}) fullWidth = false;
 
     static get styles() {
         return [
@@ -109,7 +110,7 @@ export class Button extends LitElement {
         }
 
         return html`
-                <button class='${type}'>
+                <button class='${type} ${this.fullWidth ? 'full-width' : ''}'>
                     ${ this.iconPrefix ? html`<slot name='iconPrefix'></slot>&nbsp;` : ''}
                     <span class='label'>${this.label}</span>
                     ${ this.iconSuffix ? html`&nbsp;<slot name='iconSuffix'></slot> ` : ''}
