18134491d0c6345c0a7cc1167f421baa1b91b9f5
Jayabratha Saha <js.design@outlook.com>
2019-12-04 16:37:19 +0530

    Checkbox initial done

--- components/checkbox/src/jsd-checkbox.ts
+++ components/checkbox/src/jsd-checkbox.ts
@@ -1,89 +1,90 @@
 import { LitElement, customElement, html, css, property } from 'lit-element';
 import { baseStyles } from '@jsdesign/jsd-base';
 
-@customElement('jsd-radio')
-export class Radio extends LitElement {
+@customElement('jsd-checkbox')
+export class Checkbox extends LitElement {
     @property({ type: String }) id = '';
     @property({ type: String }) name = '';
     @property({ type: String }) formId = '';
     @property({ type: String }) label = '';
     @property({ type: Boolean }) inline = false;
-    @property({ type: String, attribute: 'value', reflect: true }) selectedValue = '';
-    @property({ type: Array }) list = [];
+    @property({ type: Array, attribute: 'values', reflect: true }) selectedValues: Array<string> = [];
+    @property({ type: Array }) list: Array<string> = [];
 
     static get styles() {
         return [
             baseStyles,
             css`
-            .jsd-radio-wrapper {
+            .jsd-checkbox-wrapper {
                 padding: 0.5rem 0;
             }
     
-            .jsd-radio-wrapper .jsd-radio {
+            .jsd-checkbox-wrapper .jsd-checkbox {
                 display: block;
                 padding: 0.5rem 1.5rem 0.5rem 0;
             }
     
-            .jsd-radio-wrapper.inline .jsd-radio {
+            .jsd-checkbox-wrapper.inline .jsd-checkbox {
                 display: inline-block;
             }
     
-            .jsd-radio-wrapper .radio-input {
+            .jsd-checkbox-wrapper .checkbox-input {
                 position: absolute;
                 margin: 0;
                 opacity: 0;
             }
     
-            .jsd-radio-wrapper label {
+            .jsd-checkbox-wrapper label {
                 display: flex;
-                align-items: center;
                 color: var(--color-placeholder);
                 cursor: pointer;
                 z-index: 1;
             }
     
-            .jsd-radio-wrapper .radio-input:checked+label,
-            .jsd-radio-wrapper .jsd-radio:hover label {
+            .jsd-checkbox-wrapper .checkbox-input:checked+label,
+            .jsd-checkbox-wrapper .jsd-checkbox:hover label {
                 color: var(--color-black);
             }
     
-            .jsd-radio-wrapper label .radio {
+            .jsd-checkbox-wrapper label .checkbox {
                 position: relative;
                 height: calc(1rem + 2px);
                 width: calc(1rem + 2px);
                 margin-right: 0.5rem;
-                border-radius: 50%;
+                border-radius: 4px;
                 border: 1px solid var(--color-border);
                 background-color: var(--color-secondary);
             }
-
-            .jsd-radio-wrapper label:hover .radio {
+    
+            .jsd-checkbox-wrapper label:hover .checkbox {
                 background-color: var(--color-white);
             }
-            .jsd-radio-wrapper .radio-input:active+label .radio,
-            .jsd-radio-wrapper .radio-input:focus+label .radio {
+    
+            .jsd-checkbox-wrapper .checkbox-input:active+label .checkbox,
+            .jsd-checkbox-wrapper .checkbox-input:focus+label .checkbox {
                 background-color: var(--color-white);
                 border-color: var(--color-primary);
             }
-            .jsd-radio-wrapper .radio-input+label .radio:after {
-                content: '';
-                position: absolute;
-                height: 10px;
-                width: 10px;
-                border-radius: 50%;
-                top: calc(50% - 5px);
-                left: calc(50% - 5px);
-                border: thin solid var(--color-primary);
-                box-sizing: border-box;
+    
+            .jsd-checkbox-wrapper .checkbox-input:checked+label .checkbox {
                 background-color: var(--color-primary);
-                transform: scale(0);
-                transition: transform 0.2s;
+                border-color: var(--color-primary);
             }
     
-            .jsd-radio-wrapper .radio-input:checked+label .radio:after {
-                transform: scale(1);
+            .jsd-checkbox-wrapper .checkbox-input:checked+label .checkbox:after {
+                content: '';
+                border: solid var(--color-white);
+                border-width: 0 2px 2px 0;
+                display: inline-block;
+                padding: 4px 2px;
+                left: 5px;
+                top: 2px;
+                position: absolute;
+                transform: rotate(45deg);
             }
-            .jsd-radio-wrapper .radio-input:checked+label:hover .radio:after {
+    
+            .jsd-checkbox-wrapper .checkbox-input:checked:focus+label .checkbox,
+            .jsd-checkbox-wrapper .checkbox-input:checked+label:hover .checkbox {
                 background-color: var(--color-primary-dark);
             }
             `
@@ -95,7 +96,14 @@ export class Radio extends LitElement {
                 bubbles: true,
                 composed: true
             });
-        this.selectedValue = event.target.value;
+        let selectedValue = event.target.value;
+        let index = this.selectedValues.indexOf(selectedValue);
+        if (index !== -1) {
+            this.selectedValues.splice(index, 1);
+        } else {
+            this.selectedValues.push(selectedValue);
+        }
+        this.selectedValues = [...this.selectedValues];
         setTimeout(() => {
             this.dispatchEvent(changeEvent);
         }, 0);
@@ -103,19 +111,19 @@ export class Radio extends LitElement {
     render() {
         return html`
         <div class='label'>${this.label}</div>
-            <div id='${this.id}' class='jsd-radio-wrapper ${this.inline ? 'inline' : ''}'>
+            <div id='${this.id}' class='jsd-checkbox-wrapper ${this.inline ? 'inline' : ''}'>
             ${this.list.map((item: any) => html`
-                <div class='jsd-radio'>
+                <div class='jsd-checkbox'>
                     <input 
                         id='${this.id}-${item}'
-                        class='radio-input' 
+                        class='checkbox-input' 
                         name='${this.name}' 
-                        type="radio" 
+                        type="checkbox" 
                         value='${item}' 
-                        ?checked='${item === this.selectedValue}' 
+                        ?checked='${this.selectedValues.indexOf(item) !== -1}' 
                         @change='${this.handleSelect}'/>
                     <label for='${this.id}-${item}'>
-                        <span class='radio'></span>
+                        <span class='checkbox'></span>
                         <span>${item}</span>
                     </label>
                 </div>
