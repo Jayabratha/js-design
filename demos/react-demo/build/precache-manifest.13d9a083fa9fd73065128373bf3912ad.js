478eaf08a589c882b9ec13832105e8fcbd16ecb7
Jayabratha <js.design@outlook.com>
2020-04-07 19:14:23 +0530

    Uploading React Build

---
+++ demos/react-demo/build/precache-manifest.13d9a083fa9fd73065128373bf3912ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b984890818e989dd0398b63ec58fb57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fce2944167d1dc9b9868",
+    "url": "/static/css/main.8602edd4.chunk.css"
+  },
+  {
+    "revision": "6816d0a9049b26ab07f4",
+    "url": "/static/js/2.79235d6f.chunk.js"
+  },
+  {
+    "revision": "30a447e9656797115ed393a3044ba97a",
+    "url": "/static/js/2.79235d6f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fce2944167d1dc9b9868",
+    "url": "/static/js/main.f19390b0.chunk.js"
+  },
+  {
+    "revision": "dfcfac865b7110a29788",
+    "url": "/static/js/runtime-main.89a994c7.js"
+  },
+  {
+    "revision": "19688356b77f4e47d85b80cbec82a437",
+    "url": "/static/media/js-logo.19688356.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
