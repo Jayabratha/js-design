80e9fc27c601941ca039ffcecc92b0874bf90c18
Jayabratha <js.design@outlook.com>
2020-04-08 19:38:25 +0530

    Angular build update

--- demos/angular-demo/build/runtime-es5.js
+++ demos/angular-demo/build/runtime-es5.js
@@ -137,7 +137,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "https://jayabratha.github.io/js-design/demos/angular-demo/build";
+/******/ 	__webpack_require__.p = "https://jayabratha.github.io/js-design/demos/angular-demo/build/";
 /******/
 /******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
 /******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
